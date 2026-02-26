3ef9a594cd2ec1f6f353bf109d02bb06026f2665
Gelightful <gelopagutayao@gmail.com>
2026-02-26 13:35:00 +0800

    For deployment

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -5,7 +5,7 @@ const header = () => {
       <div className="w-full px-4 py-3 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-3">
-            <img src="/frontend/dist/LOGO.png" alt="Medicare Logo" className="w-10 h-10" />
+            <img src="/LOGO.png" alt="Medicare Logo" className="w-10 h-10" />
             <span className="text-2xl font-bold"><span className="text-[#09637E]">Medi</span>care.</span>
           </div>
         </div>
