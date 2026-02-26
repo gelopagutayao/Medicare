781491cbefefc6658f12cfd5724863c729aceac3
Gelightful <gelopagutayao@gmail.com>
2026-02-26 14:44:15 +0800

    hero image updated

--- frontend/src/pages/heropage.tsx
+++ frontend/src/pages/heropage.tsx
@@ -12,27 +12,27 @@ const Heropage = () => {
         <div className="max-w-7xl mx-auto px-6 flex flex-col lg:flex-row items-center justify-between gap-6">
           
           {/* LEFT SIDE */}
-          <div className="relative w-full lg:w-1/2 flex justify-center lg:justify-end order-2 lg:order-1">
+          <div className="relative w-full lg:w-1/2 flex justify-center">
             <img 
               src="/hero1.png" 
               alt="Doctor" 
-              className="w-48 h-48 sm:w-64 sm:h-64 md:w-80 md:h-80 lg:w-[500px] lg:h-[500px] object-contain" 
+              className="w-150 h-150 relative z-10 mr-47.5 mb-10" 
             />
           </div>
 
           {/* RIGHT SIDE */}
-          <div className="w-full lg:w-1/2 text-center lg:text-left px-4 order-1 lg:order-2 lg:mt-0 mt-6">
+          <div className="w-full lg:w-1/2 text-center lg:text-left px-2.5 -mt-30">
             
-            <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-gray-900 leading-tight">
-              Expert care, from <br className="hidden sm:block" /> the comfort of your couch
+            <h1 className="text-4xl md:text-5xl font-bold text-gray-900 leading-tight">
+              Expert care, from <br /> the comfort of your couch
             </h1>
 
-            <p className="mt-4 sm:mt-5 text-gray-600 max-w-md mx-auto lg:mx-0 text-sm sm:text-base">
+            <p className="mt-5 text-gray-600 max-w-md mx-auto lg:mx-0">
               Connect with certified specialists in under 15 minutes through our
               secure, HIPAA-compliant platform
             </p>
 
-            <div className="mt-5 sm:mt-6 flex flex-col sm:flex-row gap-3 sm:gap-4 justify-center lg:justify-start">
+            <div className="mt-6 flex flex-col sm:flex-row gap-4 justify-center lg:justify-start">
               <Link to="/book-appointment" className="bg-[#09637E] text-white px-6 py-3 rounded-lg shadow-md transition-all duration-300 hover:shadow-[0_0_20px_rgba(9,99,126,0.6)]">
                 <img src={stethoscope} alt="Stethoscope" className="w-5 h-5 inline mr-2 brightness-0 invert" /> Book Appointment
               </Link>
@@ -42,14 +42,14 @@ const Heropage = () => {
               </Link>
             </div>
 
-            <div className="mt-7 sm:mt-9.25 flex flex-col sm:flex-row gap-3 sm:gap-4 justify-center lg:justify-start items-center">
-              <div className="flex items-center border-[#09637E] gap-2 border px-4 sm:px-6 py-2 rounded-lg bg-white shadow-sm text-xs sm:text-sm">
+            <div className="mt-9.25 flex flex-col sm:flex-row gap-4 justify-center lg:justify-start items-center">
+              <div className="flex items-center border-[#09637E] gap-2 border px-6 py-2 rounded-lg bg-white shadow-sm">
                 <Star size={16} className="text-[#09637E]" />
-                <span className="font-medium">4.9/5 Rating</span>
+                <span className="text-sm font-medium">4.9/5 Rating</span>
               </div>
 
-              <div className="flex items-center gap-2 border border-[#09637E] px-3 sm:px-4 py-2 rounded-lg bg-white shadow-sm">
-                <span className="text-xs sm:text-sm font-medium">
+              <div className="flex items-center gap-2 border border-[#09637E] px-4 py-2 rounded-lg bg-white shadow-sm">
+                <span className="text-sm font-medium">
                   Over 10,000+ happy patients
                 </span>
               </div>
