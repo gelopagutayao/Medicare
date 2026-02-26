e8fad321c0e4151df90709371d81f8944e3f8bae
Gelightful <gelopagutayao@gmail.com>
2026-02-26 13:45:29 +0800

    For deployment

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -5,8 +5,15 @@ const header = () => {
       <div className="w-full px-4 py-3 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-3">
-            <img src="/LOGO.png" alt="Medicare Logo" className="w-10 h-10" />
-            <span className="text-2xl font-bold"><span className="text-[#09637E]">Medi</span>care.</span>
+            <div 
+              onClick={() => window.location.href='/'} 
+              className="flex items-center gap-2 cursor-pointer"
+            >
+              <img src="/LOGO.png" alt="Medicare Logo" className="w-10 h-10" />
+              <span className="text-2xl font-bold">
+                <span className="text-[#09637E]">Medi</span>care.
+              </span>
+            </div>
           </div>
         </div>
       </div>
